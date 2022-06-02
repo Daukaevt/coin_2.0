3d02ac00383b091454c6b214ebaec9abd566a910
Daukaevt <78514903+Daukaevt@users.noreply.github.com>
2022-06-02 07:26:11 +0300

    CoinItems clickable

--- app/src/main/java/com/wixsite/mupbam1/resume/coin20/CoinItems.kt
+++ app/src/main/java/com/wixsite/mupbam1/resume/coin20/CoinItems.kt
@@ -4,6 +4,7 @@ package com.wixsite.mupbam1.resume.coin20.ui.theme
 import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -31,6 +32,7 @@ fun ItemColumn(coinDataitem: coin_dataItem) {
     }
     Card(modifier = Modifier
         .fillMaxWidth()
+        .clickable { isExpanded=!isExpanded }
         .padding(8.dp),
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
@@ -75,11 +77,10 @@ fun ItemColumn(coinDataitem: coin_dataItem) {
             }
         }
     }
-//    Row(
-//        modifier = Modifier
-//            .padding(8.dp)
-//            .background(color = Color.White)
-//    ) {
-//                    Text(text = coinDataitem.image)
-//    }
+    if (isExpanded) {
+        Text(text = "sfjvb;waervnqklejrnvqlknrv" +
+                "arkjgbfakjrfvakjf" +
+                "a.dkjvbqrekjvh;qkJHVN" +
+                "FVNA;KJRVNA;KJLGN" )
+    }
 }
